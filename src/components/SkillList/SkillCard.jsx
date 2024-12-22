05710a6e49f7e42af2a3a972b927a1d46f2e563c
Mohsin Farooq <technicalmohsin87@gmail.com>
2024-12-22 05:26:30 +0500

    Update

--- src/components/SkillList/SkillCard.jsx
+++ src/components/SkillList/SkillCard.jsx
@@ -3,7 +3,7 @@ import { Card } from "react-bootstrap";
 export default function SkillCard({ title, description, icon }) {
   return (
     <div>
-      <Card className="px-4 py-[4rem] h-[26em] flex flex-col items-start bg-[#4A90E211] backdrop-filter backdrop-blur-[1.5px] border-[1.5px] border-[solid] border-[#FFFFFF2E] text-white rounded-[3rem]">
+      <Card className="px-4 py-[4rem] h-[26em] flex flex-col items-start bg-[#4A90E211] backdrop-filter backdrop-blur-[0.09rem] border-[0.1rem] border-[solid] border-[#FFFFFF2E] text-white rounded-[3rem]">
         <div className="cardIcon bg-[#D3E97A] text-black rounded-full p-2.5 ml-4">
           {icon}
         </div>
--- src/components/utlis/Input.jsx
+++ src/components/utlis/Input.jsx
@@ -4,19 +4,19 @@ export default function Input() {
       <input
         type="text"
         placeholder="Name"
-        className="bg-[#1A1A1A] py-[0.75rem] px-[1rem] w-full rounded-[0.25rem] placeholder:text-[0.865rem] outline-none	manrope-light" />
+        className="bg-[#1A1A1A] py-[0.75rem] px-[1rem] w-full rounded-[1rem] placeholder:text-[0.865rem] outline-none	manrope-light" />
       <input
         type="text"
         placeholder="Email"
-        className="bg-[#1A1A1A] py-[0.75rem] px-[1rem] w-full rounded-[0.25rem] placeholder:text-[0.865rem] outline-none	manrope-light mt-[1.5rem]" />
+        className="bg-[#1A1A1A] py-[0.75rem] px-[1rem] w-full rounded-[1rem] placeholder:text-[0.865rem] outline-none	manrope-light mt-[1.5rem]" />
       <input
         type="text"
         placeholder="Subject"
-        className="bg-[#1A1A1A] py-[0.75rem] px-[1rem] w-full rounded-[0.25rem] placeholder:text-[0.865rem] outline-none manrope-light mt-[1.5rem]" />
+        className="bg-[#1A1A1A] py-[0.75rem] px-[1rem] w-full rounded-[1rem] placeholder:text-[0.865rem] outline-none manrope-light mt-[1.5rem]" />
       <textarea
         type="text"
         placeholder="Message"
-        className="bg-[#1A1A1A] py-[0.75rem] px-[1rem] w-full rounded-[0.25rem] placeholder:text-[0.865rem] outline-none	manrope-light mt-[1.5rem]" />
+        className="bg-[#1A1A1A] py-[0.75rem] px-[1rem] w-full rounded-[1rem] placeholder:text-[0.865rem] outline-none	manrope-light mt-[1.5rem]" />
     </div>
   )
 }
